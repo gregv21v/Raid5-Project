e416f911b9b94aa1851ee3f8f81eb54155c8a7f6
gregv21v <gregv21v@gmail.com>
2015-05-04 11:23:37 -0400

    Moved disk names to volume

--- head/volume.h
+++ head/volume.h
@@ -4,6 +4,11 @@
 
 #include "disk.h"
 
+/*Define the disk names*/
+#define DISK_0 "disk_0"
+#define DISK_1 "disk_1"
+#define DISK_2 "disk_2"
+
 
 char * volume_load(int address);
 void volume_store(int address, char * block);
