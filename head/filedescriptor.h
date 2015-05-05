8641fbb0e8e7878ef83953cb3a3ed4f7b6c70b9c
gregv21v <gregv21v@gmail.com>
2015-05-05 17:40:10 -0400

    Update filedescriptor.h

--- head/filedescriptor.h
+++ head/filedescriptor.h
@@ -8,12 +8,12 @@
 #ifndef __FILEDESCRIPTOR_H
 #define __FILEDESCRIPTOR_H
 
-#define NAME_MAX_LENGTH 28
+#define NAME_LENGTH 28
 
 /* Each file descriptor is 38 bytes large. */
 /* That leaves 8 bytes to store the address to the next block */
 typedef struct FileDescriptor {
-	char name[NAME_MAX_LENGTH];	/* 29 bytes */
+	char name[NAME_LENGTH];	/* 29 bytes */
 	unsigned int start; /* 4 bytes: The block that the file starts on in the volume. */ 
 	unsigned int blockCount; /* 4 bytes: The number of blocks that the file takes up. */
 } file_t;
