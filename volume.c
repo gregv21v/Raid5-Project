44baa6b1a882cb8324b63bd121dc950f69acff72
gregv21v <gregv21v@gmail.com>
2015-05-05 13:23:48 -0400

    fixed indenting

--- volume.c
+++ volume.c
@@ -42,67 +42,66 @@ char * volume_load_block(int address)
 
 void volume_store_block(int address, char * block)
 {
-  int error = 0;
-  
-  /* disk setup:
-      Disk 0        Disk 1
-        0             1
-        2             3
-        4             5
-        6             7
-       ...          etc.
-  */
-  
-  /* Open Disk */
-  if(address % 2 == 0) 
-  {
-    error = open_disk(DISK_0);
-  }
-  else 
-  {
-    error = open_disk(DISK_1);
-  }
-
-  /* write to the address space (disks 0, and 1) */
-  block_write((int) (address / 2), block);
-  
-  
-  /* Close Disk */
-  if(address % 2 == 0) 
-  {
-    error = open_disk(DISK_0);
-  }
-  else 
-  {
-    error = open_disk(DISK_1);
-  }
-  
-  char * disk0Block;
-  char * disk1Block;
-  char * parityBlock;
-  
-  /* update the parity disk */
-  open_disk(DISK_0);
-    block_read((int) (address / 2), disk0Block);
-  close_disk();
-  
-  open_disk(DISK_1);
-    block_read((int) (address / 2), disk1Block);
-  close_disk();
-  
-  open_disk(DISK_2);
-    block_read((int) (address / 2), parityBlock);
-  close_disk();
-  
-  /* XOR disk 0 and 2's blocks */
-  int i = 0;
-  for(i = 0; i < 512; i++)
-  {
-      parityBlock[i] = disk0Block[i] ^ disk1Block[i];
-  }
-  
-  
-  block_write((int) (address / 2), parityBlock);
+	int error = 0;
+	
+	/* disk setup:
+		Disk 0        Disk 1
+		  0             1
+		  2             3
+		  4             5
+		  6             7
+		  ...          etc.
+	*/
+	
+	/* Open Disk */
+	if(address % 2 == 0) 
+	{
+		error = open_disk(DISK_0);
+	}
+	else 
+	{
+		error = open_disk(DISK_1);
+	}
+	
+	/* write to the address space (disks 0, and 1) */
+	block_write((int) (address / 2), block);
+	
+	
+	/* Close Disk */
+	if(address % 2 == 0) 
+	{
+		error = open_disk(DISK_0);
+	}
+	else 
+	{
+		error = open_disk(DISK_1);
+	}
+	
+	char * disk0Block;
+	char * disk1Block;
+	char * parityBlock;
+	
+	/* update the parity disk */
+	open_disk(DISK_0);
+		block_read((int) (address / 2), disk0Block);
+	close_disk();
+	
+	open_disk(DISK_1);
+		block_read((int) (address / 2), disk1Block);
+	close_disk();
+	
+	open_disk(DISK_2);
+		block_read((int) (address / 2), parityBlock);
+	close_disk();
+	
+	/* XOR disk 0 and 2's blocks */
+	int i = 0;
+	for(i = 0; i < 512; i++)
+	{
+		parityBlock[i] = disk0Block[i] ^ disk1Block[i];
+	}
+	
+	block_write((int) (address / 2), parityBlock);
 
   
   
