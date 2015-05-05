1a3e79e6e2a41645746e9b66227dcbacb70bc91d
gregv21v <gregv21v@gmail.com>
2015-05-04 21:32:06 -0400

    descriptorBlock class

---
+++ head/descriptorBlock.h
@@ -0,0 +1,27 @@
+#ifndef __DESCRIPTORBLOCK_H
+#define __DESCRIPTORBLOCK_H
+
+
+
+
+
+
+
+
+typedef struct DescriptorBlock {
+	int address; /* not saved */
+	fileDescriptor_t * descriptors[FILES_PER_BLOCK];
+	unsigned int previousBlock;
+	unsigned int nextBlock;
+} descriptorBlock_t;
+
+
+void descriptorBlock_save(descriptorBlock * block);
+descriptorBlock_t * descriptorBlock_load(int address);
+
+descriptorBlock_t * descriptorBlock_find_last();
+
+
+
+
+#endif 
