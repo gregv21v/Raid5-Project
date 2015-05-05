f1ad85e5ca2d982efbadb89072b63a067f3501ee
gregv21v <gregv21v@gmail.com>
2015-05-05 17:47:36 -0400

    Update filedescriptor.c

--- filedescriptor.c
+++ filedescriptor.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+
+#include "file.h"
+
+
+file_t * file_create() 
+{
+    file_t * newFile = (file_t *) malloc(sizeof(file_t));
+    
+    strcpy(newFile->name, "");
+    newFile->start = 0;
+    newFile->blockCount = 0;
+    
+    return newFile;
+}
