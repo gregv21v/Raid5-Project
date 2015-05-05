cbcacf9e90e7df9faf213091dbf1add94058691a
gregv21v <gregv21v@gmail.com>
2015-05-04 22:20:47 -0400

    Added commetn

--- head/descriptorblock.h
+++ head/descriptorblock.h
@@ -1,7 +1,16 @@
+
+
+
+
 #ifndef __DESCRIPTORBLOCK_H
 #define __DESCRIPTORBLOCK_H
 
 #define FILES_PER_BLOCK 18
+
+/*
+	File Descriptor Block on Disk
+	previous block address | descriptor data | next block addresss 
+*/
 
 #include "filedescriptor.h"
 
@@ -12,8 +21,9 @@ typedef struct DescriptorBlock {
 	unsigned int nextBlock;
 } descriptorBlock_t;
 
-void descriptorBlock_save(descriptorBlock * block);
+void descriptorBlock_save(descriptorBlock_t * block);
 descriptorBlock_t * descriptorBlock_load(int address);
 descriptorBlock_t * descriptorBlock_load_last();
+int descriptorBlock_find_file(descriptorBlock_t * block, char * filename); /* returns -1 if the file is not found */
 
 #endif 
