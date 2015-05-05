170319c512115a22ecc99dbf5080694ce8fcbad8
gregv21v <gregv21v@gmail.com>
2015-05-04 22:55:53 -0400

    Update volume.h

--- head/volume.h
+++ head/volume.h
@@ -1,1 +1,21 @@
 
+
+#ifndef __VOLUME_H
+#define __VOLUME_H
+
+#include "disk.h"
+
+/*Define the disk names*/
+#define DISK_0 "disk_0"
+#define DISK_1 "disk_1"
+#define DISK_2 "disk_2"
+
+
+char * volume_load(int address);
+void volume_store(int address, char * block);
+
+
+
+
+
+#endif  
