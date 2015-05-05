9f8a0779bd8ac5fc25db9a13caa33ff39c80477c
gregv21v <gregv21v@gmail.com>
2015-05-04 21:33:42 -0400

    Updated

--- head/filetable.h
+++ head/filetable.h
@@ -11,35 +11,21 @@
 #include <unistd.h>
 #include <string.h>
 
+#include "descriptorBlock.h"
+
 #define FILES_PER_BLOCK 18
 #define NAME_MAX_LENGTH 30
 
 
-/* Each file descriptor is 28 bytes large. */
-/* That leaves 8 bytes to store the address to the next block */
-typedef struct FileDescriptor {
-	char name[NAME_MAX_LENTH];	/* 29 bytes */
-	unsigned int start; /* 4 bytes: The block that the file starts on in the volume. */ 
-	unsigned int blockCount; /* 4 bytes: The number of blocks that the file takes up. */
-} fileDescriptor_t;
-
-
-typedef struct DescriptorBlock {
-	fileDescriptor_t * descriptors[FILES_PER_BLOCK];
-	unsigned int previousBlock;
-	unsigned int nextBlock;
-} descriptorBlock_t;
 
 
 
+typedef struct Table {
+	fileDescriptor_t * currentFile;
+	descriptorBlock_t * currentFileBlock;
+	descriptorBlock_t * lastFileBlock;
+} table_t;
 
-
-
-
-void filetable_add_file(list_t * table, file_t * file);
-void filetable_display(list_t * table);
-void filetable_list_files(list_t * table);
-file_t * filetable_find(list_t * table, char * filename);
 
 
 
