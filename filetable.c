251ff38150270e43030438f895b92c5da8aab596
gregv21v <gregv21v@gmail.com>
2015-05-04 22:22:47 -0400

    .

--- filetable.c
+++ filetable.c
@@ -6,44 +6,69 @@
  * Description: This file provides a file table data structure.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-
-/*
-	Saving files
-	-------------
-	Each block is 42 bytes large.
-	12 file descriptors are stored in a block.
-	The last 8 bytes store the address of the next block.
-*/
-/*
-	Important Facts
-	----------------
-	
-	
-
-*/
-
-
 
 #include "filetable.h"
 
+table_t * filetable_create()
+{
+	table_t * table = (table_t *) malloc(sizeof(table_t));
+	
+	/* find the last descriptor block */
+	table->lastFileBlock = descriptorBlock_load_last();
+	/* TODO: Add condition for empty file table */
+	table->currentFileBlock = descriptorBlock_load(0); 
+	table->currentFile = NULL;
+}
 
 
-/*
-	fileBlock 
-	previous block address | descriptor data | next block addresss 
+int filetable_find_last_free(table_t * table)
+{
+	int index = descriptorBlock_find_file(table->lastFileBlock, "");
+	if(index != -1) 
+		return table->lastFileBlock->descriptors[index]->start 
+			+ table->lastFileBlock->descriptors[index]->blockCount;
+}
+
+void filetable_add_file(table_t * table, char * name, int blockCount)
+{
+	int index = descriptorBlock_find_file(table->lastFileBlock, "");
+	
+	if(index != -1) 
+	{
+		memcpy(table->lastFileBlock->descriptors[index]->name, name, MAX_NAME_LENGTH);
+		table->lastFileBlock->descriptors[index]->start = filetable_find_last_free(table);
+		table->lastFileBlock->descriptors[index]->blockCount = blockCount;
+	}
+}
 
 
-*/
-/* 
-	Finding the next avaliable address:
-		
 
+/* private function */
+descriptorBlock_t * filetable_find_last_descriptor_block()
+{
+	descriptorBlock_t * last = (descriptorBlock_t *) malloc(sizeof(descriptorBlock_t));
+	descriptorBlock_t * current
+	
+	
+	descriptorBlock = load_descriptor_block(currentBlock);
+	
+	/* Check to see if the file is in this block */
+	/* "" means you have an empty file descriptor */
+	for(i = 0; i < FILES_PER_BLOCK && !found; i++) 
+	{
+		/* you have found the last file */
+		if(strcmp(descriptorBlock->descriptors[i]->name, "") == 0) 
+		{
+			index = i;
+			found = 1;
+		}
+	}
+	
+}
 
 /*
 	Adds a file to the list
+	Whenever you add a new file, add an empty file after it. ()
 	pre: Name should be of length NAME_MAX_LENGTH
 	post: Adds the file to the end of the table.
 */
@@ -69,7 +94,7 @@ int filetable_add_file(char * name, int blockCount)
 	*/
 	while(!found) 
 	{
-		descriptorBlock = convertToDescriptorBlock(currentBlock);
+		descriptorBlock = load_descriptor_block(currentBlock);
 		
 		/* Check to see if the file is in this block */
 		/* "" means you have an empty file descriptor */
@@ -135,49 +160,45 @@ int filetable_add_file(char * name, int blockCount)
 	
 }
 
-char * volume_load(int blockNumber)
-{
-		
-	
-	return NULL;
-}
-
-
-
-
-void volume_store(int address, char * data)
+file_t * nextFile(table_t * table)
 {
 	
 }
 
-descriptorBlock_t * convertToDescriptorBlock(char * block)
+
+
+descriptorBlock_t * load_descriptor_block(int address)
 {
+	char * buffer = volume_load(address);
+	
 	descriptorBlock_t * block = (descriptorBlock_t *) malloc(sizeof(descriptorBlock_t));
+	
+	block->address = address;
 	
 	int offset = 0;
 	
 	/* read the first 4 bytes as the address of the previous block */
-	memcpy(block->previousBlock, block, 4);
+	memcpy(block->previousBlock, buffer, 4);
 	offset += 4;
 	
 	/* Load the current block into the file descriptor array */
 	for(i = 0; i < FILES_PER_BLOCK; i++) 
 	{
 		/* Copy the name from the block */
-		memcpy(descriptors[i]->name, currentBlock + offset, 29);
+		memcpy(descriptors[i]->name, buffer + offset, 29);
 		offset += 29;
 		
 		/* Copy the start block address from the block */
-		memcpy(descriptors[i]->start, currentBlock + offset, 4);
+		memcpy(descriptors[i]->start, buffer + offset, 4);
 		offset += 4;
 		
 		/* Copy the block cout from the block */
-		memcpy(descriptors[i]->blockCount, currentBlock + offset, 4);
+		memcpy(descriptors[i]->blockCount, buffer + offset, 4);
 		offset += 4;
 	}
 	
 	/* read the last 4 bytes as the address of the next block */
-	memcpy(block->nextBlock, block+offset, 4);
+	memcpy(block->nextBlock, buffer+offset, 4);
 	
 	return block;
 }
