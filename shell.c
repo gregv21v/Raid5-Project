662607e8ab900c909e6c3f8161901e2638d911b5
gregv21v <gregv21v@gmail.com>
2015-05-05 15:33:40 -0400

    Update shell.c

--- shell.c
+++ shell.c
@@ -46,7 +46,7 @@ int main(int argc, char **argv)
 		
 		char * testBlock = (char *) malloc(512);
 		char * str = "Testing...";
-		strcpy(testBlock, str, strlen(str));
+		strcpy(testBlock, str);
 		
 		printf("%s", testBlock);
 		
