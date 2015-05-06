dfbe369a81223f6872ded6e679f7eee007afee0f
gregv21v <gregv21v@gmail.com>
2015-05-06 19:32:28 -0400

    Header comments

--- filedescriptor.c
+++ filedescriptor.c
@@ -8,7 +8,11 @@
  
  
  
- 
+  /* 
+        Creates a file
+        Pre: None
+        Post: Allocates memory for a file descriptor
+  */ 
  file_t * file_create()  
  { 
      file_t * newFile = (file_t *) malloc(sizeof(file_t)); 
@@ -20,13 +24,21 @@
      return newFile; 
  } 
  
- 
+ /* 
+        Destroys a file
+        Pre: None
+        Post: Frees memory for the file descriptor
+  */ 
  void file_destroy(file_t * file) 
  { 
      free(file); 
  } 
  
- 
+ /* 
+        Displays all the data of a file descriptor
+        Pre: None
+        Post: Displays the file descriptor data
+  */ 
  void file_display(file_t * file) 
  { 
      printf("%s |  %d  |  %d  \n", file->name, file->start, file->blockCount); 
