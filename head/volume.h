ccac6a94ff22a4c3019c7054ecbd31b1a9cb1100
gregv21v <gregv21v@gmail.com>
2015-05-06 19:25:52 -0400

    SPACING!!!

--- head/volume.h
+++ head/volume.h
@@ -1,22 +1,15 @@
+#ifndef __VOLUME_H 
+#define __VOLUME_H 
  
+#include "disk.h" 
  
- #ifndef __VOLUME_H 
- #define __VOLUME_H 
- 
- 
- #include "disk.h" 
- 
- 
- /*Define the disk names*/ 
- #define DISK_0 "disk_0" 
- #define DISK_1 "disk_1" 
- #define DISK_2 "disk_2" 
- 
- 
- void volume_display_block_raw(unsigned int address); 
- char * volume_load_block(unsigned int address); 
- void volume_store_block(unsigned int address, char * block); 
- 
- 
+/*Define the disk names*/ 
+#define DISK_0 "disk_0" 
+#define DISK_1 "disk_1" 
+#define DISK_2 "disk_2" 
 
- #endif   
+void volume_display_block_raw(unsigned int address); 
+char * volume_load_block(unsigned int address); 
+void volume_store_block(unsigned int address, char * block); 
+ 
+#endif   
