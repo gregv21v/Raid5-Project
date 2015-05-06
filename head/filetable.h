1bff4dc44abb537902b80f843e2ff5e5c8821147
gregv21v <gregv21v@gmail.com>
2015-05-06 18:32:14 -0400

    Added comments
    Comments about what has been tested or not.
    And changed the header comment

--- head/filetable.h
+++ head/filetable.h
@@ -3,7 +3,7 @@
  * Author: Gregory Venezia
  * Date: 4/27/2015
  * Course: CSC3320
- * Description: This file provides a file table data structure.
+ * Description: This file provides a VTOC data structure.
  */
 #ifndef __FILETABLE_H
 #define __FILETABLE_H
@@ -18,14 +18,14 @@ typedef struct Table {
 	descriptorBlock_t * lastFileBlock;
 } table_t;
 
-
 table_t * filetable_create(); /* initializes last file block */
+				/* tested */
+void filetable_destroy(); /* not tested */
 int filetable_add_file(table_t * table, char * name, int blockCount); /* adds to the end of the last file block */
-file_t * filetable_find_file(table_t * table, char * name);
-void filetable_remove_file(table_t * table, char * name);
-void filetable_list_files(table_t * table);
-void filetable_display_details(table_t * table);
-
-
+									/* tested */
+file_t * filetable_find_file(table_t * table, char * name); /* tested */
+void filetable_remove_file(table_t * table, char * name); /* not tested */
+void filetable_list_files(table_t * table); /* tested */
+void filetable_display_details(table_t * table); /* tested */
 
 #endif	/* __FILETABLE_H */
