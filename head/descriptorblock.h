a6c0410224da1b4446d49181202a1d6d8e874add
gregv21v <gregv21v@gmail.com>
2015-05-06 18:28:16 -0400

    Update descriptorblock.h

--- head/descriptorblock.h
+++ head/descriptorblock.h
@@ -34,7 +34,7 @@
  } descriptorBlock_t; 
 
  descriptorBlock_t * descriptorBlock_create(int address); /* tested */ 
- void descriptorBlock_destroy();  
+ void descriptorBlock_destroy();  /* tested */
  void descriptorBlock_store(descriptorBlock_t * block); /* tested */ 
  descriptorBlock_t * descriptorBlock_load(int address); /* tested */ 
  descriptorBlock_t * descriptorBlock_load_last(); /* tested */ 
