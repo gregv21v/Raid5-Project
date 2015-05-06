c86131cca89377ef55c2f2c2b28abe33fccee915
gregv21v <gregv21v@gmail.com>
2015-05-06 15:23:32 -0400

    Update shell.c

--- shell.c
+++ shell.c
@@ -49,9 +49,9 @@ int main(int argc, char **argv)
 
 		table_t * table = filetable_create();
 		
-		filetable_add_file(table->lastFileBlock, "Test", 10);
-		filetable_add_file(table->lastFileBlock, "Test1", 10);
-		filetable_add_file(table->lastFileBlock, "Test3", 10);
+		filetable_add_file(table, "Test", 10);
+		filetable_add_file(table, "Test1", 10);
+		filetable_add_file(table, "Test3", 10);
 		
 		filetable_display_details(table);
 		
