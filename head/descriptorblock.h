216129af4cccd33a57a63485d048bbe940fc2b57
gregv21v <gregv21v@gmail.com>
2015-05-06 19:27:29 -0400

    Sorted the lines of code

--- head/descriptorblock.h
+++ head/descriptorblock.h
@@ -7,13 +7,6 @@
  	stored in one block of memory in the volume.
  */
  
-
- 
- #ifndef __DESCRIPTORBLOCK_H 
- #define __DESCRIPTORBLOCK_H 
- 
- #define FILES_PER_BLOCK 14 
-
  /* 
  	File Descriptor Block on Disk 
  	previous block address | descriptor data | next block addresss  
@@ -24,16 +17,22 @@
  	FILES_DESCRIPTOR_SIZE = 36 
  */ 
 
+ 
+ #ifndef __DESCRIPTORBLOCK_H 
+ #define __DESCRIPTORBLOCK_H 
+ 
  #include "filedescriptor.h" 
  
+ #define FILES_PER_BLOCK 14 
+
  typedef struct DescriptorBlock { 
- 	int address; /* not saved */ 
+ 	unsigned int address; /* not saved */ 
  	file_t * descriptors[FILES_PER_BLOCK]; /* these are the files */
  	unsigned int previousBlock; /* address of next block of the file table */
  	unsigned int nextBlock; /* address of the block that came before this in the file table */
  } descriptorBlock_t; 
 
- descriptorBlock_t * descriptorBlock_create(int address); /* tested */ 
+ descriptorBlock_t * descriptorBlock_create(unsigned int address); /* tested */ 
  void descriptorBlock_destroy();  /* tested */
  void descriptorBlock_store(descriptorBlock_t * block); /* tested */ 
  descriptorBlock_t * descriptorBlock_load(int address); /* tested */ 
@@ -46,7 +45,7 @@
 
  void descriptorBlock_attach(descriptorBlock_t * block, descriptorBlock_t * blockToAttach); /* tested */ 
  
- int descriptorBlock_add_file(descriptorBlock_t * block, char * filename, int blockCount); /* tested */
+ int descriptorBlock_add_file(descriptorBlock_t * block, char * filename, unsigned int blockCount); /* tested */
  int descriptorBlock_find_last_free(descriptorBlock_t * block); /* tested */
  
  
