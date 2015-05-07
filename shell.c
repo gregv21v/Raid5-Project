5983da2aa5ea04e52c2519fdeba7e48d764d3ae0
ajjoyal01 <ajjoyal01@users.noreply.github.com>
2015-05-07 00:42:36 -0400

    Update shell.c

--- shell.c
+++ shell.c
@@ -153,7 +153,7 @@ int main(int argc, char **argv)
 			{
 				int startBlock = file->start;
 				int currentBlock = startBlock;
-				int endBlock = startBlock + (file->blockCount);
+				int endBlock = startBlock + 22;/*(file->blockCount);*/
 				while(currentBlock<=endBlock)/*Iterate through the blocks of the file*/
 				{
 					volume_display_block_raw(currentBlock);
